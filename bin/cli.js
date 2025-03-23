8e3e8b28a5b38ae4533da43add614252283efb7e
David <davidgonsal@gmail.com>
2025-03-23 18:44:43 +0200

    Refactor CLI to use dynamic route generation options from svelte.config.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,24 @@
 #!/usr/bin/env node
 
-import { generateRoutes } from "../dist/index.js";
+import { generateRoutes } from "../src/index.js";
+import path from "path";
+import fs from "fs";
 
-generateRoutes();
+/**
+ * @typedef {import('../src/types.js').RouteGeneratorOptions} RouteGeneratorOptions
+ */
+
+const cwd = process.cwd();
+const configFile = path.join(cwd, "svelte.config.js");
+
+/** @type {RouteGeneratorOptions} */
+const options = {};
+
+if (fs.existsSync(configFile)) {
+    const config = await import(configFile);
+    if (config.default?.kit?.files?.routes) {
+        options.routesDir = config.default.kit.files.routes;
+    }
+}
+
+generateRoutes(options);
